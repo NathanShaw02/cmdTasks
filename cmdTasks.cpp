d91e09cf5d74c5a4e4cb76c48f072e260e9da411
NathanShaw02 <nathanshaw02@outlook.com>
2025-04-14 22:21:20 +0100

    created framework to build on

---
+++ cmdTasks.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void importTasks(vector<string>* taskVector){
+    //use task pointer to shove the tasks straight into the vector 8)
+
+
+}
+
+
+int main(int argc, char* argv[]){ //main natively processes cmd line stuff into [numberOfArgs,argsPassed]
+
+    if(argc > 3){
+        //repurpose this code at the end when you catch exception that no valid commands were passed
+        cout<<"Parameters:\n -g / get_tasks,\n -a / add_task <taskText>,\n -r / rm_task <taskNum>"<<endl;
+
+        return -1;
+    }else{
+        string mainArg = argv[1]; //args start at index 1 :/
+
+        if(mainArg=="get_tasks"||mainArg=="-g"){
+            //add number to task that you print
+                //task numbers not stored in task as part of it, just added for use
+
+        }else if(mainArg=="add_tasks"||mainArg=="-a"){
+            string taskText;
+            //use stringbuilder to add all the rest of the arguments 
+
+
+        }else if(mainArg=="rm_task"||mainArg=="-r"){
+            int taskNum = stoi(argv[2]);
+
+            //remove task number
+                //add to completed tasks txt
+
+        }
+    }
+
+
+
+    return 0;
+}
